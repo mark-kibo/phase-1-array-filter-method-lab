3fe1c4974aed33ab3bf565c37dc97e523acee7d6
mark-kibo <kibochamark@gmail.com>
2023-07-14 09:36:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+const findMatching=(drivers, string)=>{
+    let result = drivers.filter(driver => driver.toLowerCase().includes(string.toLowerCase()));
+    return result;
+};
+const fuzzyMatch=(drivers, string)=>{
+    let result=drivers.filter(driver => driver.startsWith(string));
+    return result;
+};
+const matchName=(drivers, string)=>{
+    let result=drivers.filter(driver=> driver.name == string);
+    return result;
+};
